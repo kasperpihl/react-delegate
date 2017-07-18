08b293f90a080b000acfe60beb57ff379a16c84c
Kasper Pihl <kasperpihl@users.noreply.github.com>
2017-07-18 13:36:21 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+export function setupCachedCallback(method, ctx) {
+  const cachedMethod = {};
+  return function cachedCallback(id) {
+    id = id || '';
+    if(typeof(id) === 'object') {
+      id = JSON.stringify(id);
+    }
+    if (!cachedMethod[id]) {
+      const args = Array.from(arguments);
+      cachedMethod[id] = method.bind(ctx, ...args);
+    }
+    return cachedMethod[id];
+  };
+}
+
+export function setupDelegate(obj, ...delegateMethods) {
+  let globals = [];
+
+  const delegate = obj && (obj.delegate || (obj.props && obj.props.delegate));
+  obj.callDelegate = function callDelegate(name, ...rest) {
+    if (delegate && typeof delegate[name] === 'function') {
+      return delegate[name](...globals.concat(rest));
+    }
+
+    return undefined;
+  };
+
+  delegateMethods.forEach((funcName) => {
+    if(typeof funcName === 'string'){
+      obj[funcName] = obj.callDelegate.bind(null, funcName);
+      obj[`${funcName}Cached`] = setupCachedCallback(obj[funcName]);
+    }
+  })
+
+  return {
+    setGlobals: (...globalArgs) => {
+      globals = globals.concat(globalArgs);
+    }
+  }
+}
